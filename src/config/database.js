e1cc489db1d69d3d2bc6ff858a0cb3b1210c65b2
Carvalho, Vinicius Luiz <carvalho.viniciusluiz@gmail.com>
2020-01-31 16:25:19 -0300

    fix: Path storage

--- src/config/database.js
+++ src/config/database.js
@@ -6,7 +6,7 @@ module.exports = {
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
   dialect: process.env.DB_DIALECT || 'postgres',
-  storage: './__testes__/database.sqlite',
+  storage: './__tests__/database.sqlite',
   logging: false,
   define: {
     timestamps: true,

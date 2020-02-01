01a4f19220058c867ff2e2855218bb23f3f5ed79
Carvalho, Vinicius Luiz <carvalho.viniciusluiz@gmail.com>
2020-02-01 14:14:31 -0300

    feat: Refactor with factory girl

--- __tests__/integration/user.test.js
+++ __tests__/integration/user.test.js
@@ -1,16 +1,45 @@
-import { request } from 'supertest';
+import request from 'supertest';
 import app from '../../src/app';
 
+import factory from '../factories';
+import truncate from '../util/truncate';
+
 describe('User', () => {
+  beforeEach(truncate);
+
+  it('should check user password is valid', async () => {
+    const user = await factory.create('User', {
+      password: '123456',
+    });
+
+    const checkPassword = await user.checkPassword('123456');
+
+    expect(checkPassword).toBe(true);
+  });
+
   it('should be able to register', async () => {
+    const user = await factory.attrs('User');
+
     const response = await request(app)
       .post('/users')
-      .send({
-        name: 'Vinicius Carvalho',
-        email: 'carvalho.viniciusluiz@gmail.com',
-        password: '123456',
-      });
+      .send(user);
 
+    expect(response.status).toBe(200);
     expect(response.body).toHaveProperty('id');
+  });
+
+  it('should not be able to registro with duplicated email', async () => {
+    const user = await factory.attrs('User');
+    try {
+      await request(app)
+        .post('/users')
+        .send(user);
+
+      await request(app)
+        .post('/users')
+        .send(user);
+    } catch (e) {
+      expect(e.status).toBe(400);
+    }
   });
 });

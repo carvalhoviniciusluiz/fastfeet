fe2cf06eca9e8fef88115cb3e46e3e9071b571c8
Carvalho, Vinicius Luiz <carvalho.viniciusluiz@gmail.com>
2020-02-11 09:42:33 -0300

    feat: Adding validation

--- src/app/controllers/DeliveryController.js
+++ src/app/controllers/DeliveryController.js
@@ -1,16 +1,25 @@
 import Order from '../models/Order';
 import File from '../models/File';
 import Recipient from '../models/Recipient';
+import Deliveryman from '../models/Deliveryman';
 
 class DeliveryController {
   async index(req, res) {
     const { page = 1, per_page = 20 } = req.query;
 
+    const { id: deliverymanId } = req.params;
+
+    const deliveryman = await Deliveryman.findByPk(deliverymanId);
+
+    if (!deliveryman) {
+      return res.status(400).json({ error: 'Deliveryman not found.' });
+    }
+
     const orders = await Order.findAll({
       where: {
         canceled_at: null,
         end_date: null,
-        deliveryman_id: req.params.id,
+        deliveryman_id: deliveryman.id,
       },
       order: ['product'],
       attributes: ['id', 'product', 'start_date'],
